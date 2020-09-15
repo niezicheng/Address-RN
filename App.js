e83fffde19c3abc724d1ef3b76bff64225260d60
niezicheng <2429705898@qq.com>
2020-09-15 18:26:43 +0800

    feat: add pull refresh and loading more

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, {createRef} from 'react';
+import React, {createRef, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,8 +14,9 @@ import {
   Text,
   StatusBar,
   SectionList,
-  TouchableHighlight,
-  Toast,
+  TouchableOpacity,
+  RefreshControl,
+  ActivityIndicator,
 } from 'react-native';
 // import Constants from 'expo-constants';
 import {findIndex} from 'lodash';
@@ -32,9 +33,12 @@ const Item = ({title}) => {
 
 const App = () => {
   const sectionRef = createRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const [addressData, setAddressData] = useState(jsonData);
+  const [footerStatus, setFooterStatus] = useState(0);
 
+  // 根据对应索引滑动定位
   const scrollToLocation = (indexKey) => {
-    // Toast.info(indexKey);
     const sectionIndex = findIndex(jsonData, (item) => {
       return item.title === indexKey;
     });
@@ -43,17 +47,62 @@ const App = () => {
     }
   };
 
+  // 侧边索引定位
   const sliderDataView = sliderData.map((item) => {
     return (
-      <TouchableHighlight
+      <TouchableOpacity
         key={item}
+        activeOpacity={0.8}
         onPress={() => {
           scrollToLocation(item);
         }}>
         <Text style={styles.letterStyle}>{item}</Text>
-      </TouchableHighlight>
+      </TouchableOpacity>
     );
   });
+
+  // 上拉刷新执行函数
+  const refreshData = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  };
+
+  // 下拉加载更多的数据信息
+  const loadMoreData = () => {
+    // setFooterStatus(1);
+    // setFooterStatus(2);
+    // setFooterStatus(0);
+  };
+
+  // 底部加载更多样式
+  const renderFooterMoreLoading = () => {
+    if(footerStatus === 1) {
+      return (
+        <View style={styles.loadingMore}>
+          <ActivityIndicator
+            // style={}
+            size="large"
+            color="red"
+            animating={true}
+          />
+          <Text style={styles.footerText}>正在加载...</Text>
+        </View>
+      );
+    }
+    if(footerStatus === 2) {
+      return (
+      <View style={styles.loadingMore}>
+        <Text style={styles.footerText}>已经到底了...</Text>
+      </View>)
+    }
+    return (
+      <View style={styles.loadingMore}>
+        <Text style={styles.footerText} />
+      </View>
+    )
+  }
 
   return (
     <>
@@ -61,14 +110,35 @@ const App = () => {
       <SafeAreaView style={styles.container}>
         <SectionList
           ref={sectionRef}
-          sections={jsonData}
-          keyExtractor={(item, index) => item + index}
+          sections={addressData} // 数据源
+          keyExtractor={(item, index) => item + index} // 只设置每行 item 的 key 值
           renderItem={({item, index}) => (
-            <Item key={`item_${index}`} title={item} />
+            <Item key={`item_${index}`} title={item} /> // 每个 section 中的 单个 item 列表项
           )}
           renderSectionHeader={({section: {title}}) => (
-            <Text style={styles.header}>{title}</Text>
+            <Text style={styles.header}>{title}</Text> // 每个 section 部分头部渲染
           )}
+          // 下拉刷行
+          // refreshing={isLoading}
+          // onRefresh={() => {
+          //   refreshData();
+          // }}
+          refreshControl={
+            <RefreshControl
+              title="Loading" // android 中无效
+              colors={['red']} // android
+              tintColor="red" // ios
+              titleColor="red"
+              refreshing={isLoading}
+              onRefresh={() => {
+                refreshData();
+              }}
+            />
+          }
+          onEndReached={() => {
+            loadMoreData();
+          }}
+          ListFooterComponent={() => renderFooterMoreLoading()}
         />
         <View style={styles.sliderNav}>{sliderDataView}</View>
       </SafeAreaView>
@@ -103,6 +173,15 @@ const styles = StyleSheet.create({
   },
   letterStyle: {
     padding: 3,
+  },
+  loadingMore: {
+    alignItems: 'center',
+  },
+  footerText: {
+    fontSize: 16,
+    color: '#CCCCCC',
+    paddingTop: 5,
+    paddingBottom: 10,
   },
 });
 
